1c844495a92fd83e6b03f65ffabc752dc354794c
johnmasiello <johnmasiello@users.noreply.github.com>
2026-08-28 22:58:43 -0700

    Add: validate-parentheses - submission-4

---
+++ Data Structures & Algorithms/validate-parentheses/submission-4.java
@@ -0,0 +1,32 @@
+class Solution {
+    private static final int[] CLOSING_MAP;
+
+    static {
+        CLOSING_MAP = new int[128];
+        CLOSING_MAP['('] = ')';
+        CLOSING_MAP['{'] = '}';
+        CLOSING_MAP['['] = ']';
+    }
+
+    public boolean isValid(String s) {
+        // "([{}])"
+        Deque<Character> stack = new ArrayDeque<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if ("({[".contains("" + c)) {
+                stack.push(c);
+            } else if (")}]".contains("" + c)) {
+                if (stack.isEmpty() || !isClosingBracket(stack.pop(), c)) { // satisifies 1. Every open bracket is closed by the same type of close bracket
+                    // ...and 2. Open brackets are closed in the correct order.
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty(); // satisfies: 3. Every close bracket has a corresponding open bracket of the same type.
+    }
+    static boolean isClosingBracket(char openBracket, char closedBracket) {
+        return CLOSING_MAP[openBracket] == closedBracket;
+    }
+}
