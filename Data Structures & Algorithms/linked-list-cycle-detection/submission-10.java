25aeaa20289e83744429db9dcdf38ae66b9191c7
johnmasiello <johnmasiello@users.noreply.github.com>
2026-09-07 14:30:08 -0700

    Add: linked-list-cycle-detection - submission-10

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-10.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+/* Algo:
+
+  Visit all nodes you can. If a node is revisited, then it is a cyclic list. An empty list is vacuously non cyclic.
+*/
+class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next;
+
+            if (slow == fast) {
+                return true;
+            }
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return false;
+    }
+}
