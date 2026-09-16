ed896eb9260d011105d2547062d80d979e8ee4fc
johnmasiello <johnmasiello@users.noreply.github.com>
2026-09-16 13:54:24 -0700

    Add: subtree-of-a-binary-tree - submission-7

---
+++ Data Structures & Algorithms/subtree-of-a-binary-tree/submission-7.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    // Using a linear serialization approach as first step and second step use optmized String.indexOf for 
+    // time complexity O(N + M) and space complexity O(N + M)
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if (subRoot == null) return true;
+        return serializeTree(root).indexOf(serializeTree(subRoot)) > -1;
+    }
+
+    // Time O(N) Space O(1)
+    public String serializeTree(TreeNode root) {
+        if (root == null) {
+            return "n";
+        }
+        return "" + root.val + "," + serializeTree(root.left) + "," + serializeTree(root.right);
+    }
+}
